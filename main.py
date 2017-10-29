c8f4fdf2dde7ae22e8291bd68d243624d2f1cf19
saryoni23 <saryoni23@users.noreply.github.com>
2017-10-29 10:30:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -68,8 +68,6 @@ folium.Marker(
     icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
-
-
 folium.Marker(
     location=[-7.258340, 112.751175],
     popup='Kampoeng Stik',
@@ -140,31 +138,31 @@ folium.Marker(
 folium.Marker(
     location=[-7.280613, 112.770057],
     popup='KFC Manyar',
-    icon=folium.Icon(color='green')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.279612, 112.766720],
     popup='Pizza Hut Manyar',
-    icon=folium.Icon(color='red', icon='info')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.277314, 112.766087],
     popup='Boncafe Manyar',
-    icon=folium.Icon(color='red', icon='info')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.280176, 112.768930],
     popup='Ayam Bakar Primarasa',
-    icon=folium.Icon(color='red', icon='info')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.278272, 112.766549],
     popup='Ikan Bakar Cianjur',
-    icon=folium.Icon(color='green')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
@@ -182,141 +180,136 @@ folium.Marker(
 folium.Marker(
     location=[-7.279868, 112.764027],
     popup='Gramedia Manyar',
-    icon=folium.Icon(color='red', icon='info')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.280102, 112.772460],
     popup='BCA KCP Kertajaya',
-    icon=folium.Icon(color='red', icon='info')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
-
-
 
 folium.Marker(
     location=[-7.313033, 112.728353],
     popup='Bank BTN',
-    icon=folium.Icon(color='chocolate')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.312661, 112.727430],
     popup='Gedung GEMA UNESA',
-    icon=folium.Icon(color='green')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.311842, 112.726593],
     popup='Pascasarjana Unesa',
-    icon=folium.Icon(color='red')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.314024, 112.727618],
     popup='Lapangan UNESA',
-    icon=folium.Icon(color='pink')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 
 folium.Marker(
     location=[-7.314168, 112.726180],
     popup='RanUnesa',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 
 folium.Marker(
     location=[-7.314370, 112.726754],
     popup='Kafe Unesa',
-    icon=folium.Icon(color='black')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 
 folium.Marker(
     location=[-7.314583, 112.727419],
     popup='UNESA Library',
-    icon=folium.Icon(color='yellow')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.314748, 112.728095],
     popup='Pusat Bahasa Universitas Negeri Surabaya',
-    icon=folium.Icon(color='light green')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.313785, 112.725343],
     popup='Masjid Unesa Ketintang',
-    icon=folium.Icon(color='blue')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.315482, 112.724560],
     popup='Unesa University Press',
-    icon=folium.Icon(color='light blue')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.315812, 112.724372],
     popup='Lembaga Penjaminan Mutu Pendidikan (LPMP) Jawa Timur',
-    icon=folium.Icon(color='dark blue')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.316047, 112.748636],
     popup='Plasa Marina Surabaya',
-    icon=folium.Icon(color='chocolate')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.316003, 112.748831],
     popup='Adira',
-    icon=folium.Icon(color='green')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.315823, 112.748923],
     popup='Axioo',
-    icon=folium.Icon(color='gold')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.316344, 112.748537],
     popup='Galeri Indosat Plaza Marina',
-    icon=folium.Icon(color='pink')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
-
 
 folium.Marker(
     location=[-7.316738, 112.748698],
     popup='BII ATM Sinar Fontana Surabaya',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
-
 
 folium.Marker(
     location=[-7.316344, 112.748210],
     popup='McDonalds',
-    icon=folium.Icon(color='black')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
-
 
 folium.Marker(
     location=[-7.315937, 112.748605],
     popup='HokBen Plasa Marina',
-    icon=folium.Icon(color='yellow')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.316298, 112.748005],
     popup='Samsung Partner Plaza - PEACE CELL - PLASA MARINA',
-    icon=folium.Icon(color='green')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.316024, 112.749291],
     popup='Parkir Timur Plaza Marina',
-    icon=folium.Icon(color='blue')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
@@ -325,75 +318,73 @@ folium.Marker(
     icon=folium.Icon(color='blue')
 ).add_to(bocahmicin) 
 
-
-
 folium.Marker(
     location=[-7.222785, 112.735106],
     popup='Tokoraket.com',
-    icon=folium.Icon(color='purple')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.220992, 112.732998 ],
     popup='Perak Photo',
-    icon=folium.Icon(color='white')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.221663, 112.731764 ],
     popup='PT. Pelayaran Meratus',
-    icon=folium.Icon(color='red')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.218842, 112.730257 ],
     popup='GAMA-Group',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 
 folium.Marker(
     location=[-7.215881, 112.729800 ],
     popup='Sami Jaya',
-    icon=folium.Icon(color='pink')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 
 folium.Marker(
     location=[-7.214343, 112.728389 ],
     popup='Toko Abrilia',
-    icon=folium.Icon(color='black')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 
 folium.Marker(
     location=[-7.213327, 112.727268 ],
     popup='Yonif 1 Marinir',
-    icon=folium.Icon(color='yellow')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.210098, 112.723294 ],
     popup='Cumi Hitam 19',
-    icon=folium.Icon(color='light green')
+   icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.210042, 112.722781 ],
     popup='Pelni Surya',
-    icon=folium.Icon(color='blue')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.206868, 112.721786 ],
     popup='PT. Petro Andalan Nusantara',
-    icon=folium.Icon(color='light blue')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.198897, 112.731820],
     popup='Terminal Jamrud',
-    icon=folium.Icon(color='dark blue')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 
@@ -406,31 +397,31 @@ folium.Marker(
 folium.Marker(
     location=[-7.311664, 112.795814],
     popup='Warkop Pak Salem',
-    icon=folium.Icon(color='green')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.311310, 112.796128],
     popup='Mie Ayam & Ceker Pak Men',
-    icon=folium.Icon(color='red', icon='info')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.311185, 112.795961],
     popup='Aifa Palapa',
-    icon=folium.Icon(color='red', icon='info')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.311677, 112.796321],
     popup='Family Telur',
-    icon=folium.Icon(color='red', icon='info')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.311877, 112.796691],
     popup='Warkop Karambol',
-    icon=folium.Icon(color='green')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
@@ -448,13 +439,13 @@ folium.Marker(
 folium.Marker(
     location=[-7.311568, 112.795379],
     popup='Iwan Elektronik',
-    icon=folium.Icon(color='red', icon='info')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.311558, 112.795186],
     popup='Zahara Elektronik',
-    icon=folium.Icon(color='red', icon='info')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 bocahmicin
@@ -462,191 +453,185 @@ bocahmicin
 folium.Marker(
     location=[-7.195429, 112.739471 ],
     popup='Monumen Jalesveva Jayambe',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.196121, 112.742958  ],
     popup='KRI Dewa Ruci',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.195361, 112.743256 ],
     popup='Gd. PTA',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.195426, 112.744312 ],
     popup='Diskomlekal Koarmatim',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 
 folium.Marker(
     location=[-7.194862, 112.745514 ],
     popup='Bengkel Fasharkan Surabaya',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 
 folium.Marker(
     location=[-7.197573, 112.743774  ],
     popup='E Wharf',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 
 folium.Marker(
     location=[-7.196988, 112.731659 ],
     popup='KopiLot Surabaya ',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.198126, 112.732229 ],
     popup='PT. Pelindo 3',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.196694, 112.732723 ],
     popup='Food Court SNQ',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.196721, 112.733404 ],
     popup='The Markonah Resto & Bar',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.224919, 112.787856],
     popup='Air Mancur Taman Surabaya',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.224797, 112.787975],
     popup='Warkop81',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.223414, 112.787488],
     popup='Markas SDW Group',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.222840, 112.787331],
     popup='Mushola Hikudritolilah',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.222187, 112.787522],
     popup='TAMAN PENDIDIKAN AL-QURAN DARUL FALACH',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.221971, 112.787402],
     popup='Warung Cak Munir',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.221455, 112.787368],
     popup='Zubaidah Borneo Fried Chiken',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.220610, 112.787384],
     popup='Masjid Al - Mabrur',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.220558, 112.787164],
     popup='Madrasah Ibtidaiyah Ribath Darut Tauhid',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.220796, 112.786961],
     popup='warkop GTT',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.256237, 112.751363],
     popup='Kelurahan Ketabang',
-    icon=folium.Icon(color='gray')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin) 
 
 folium.Marker(
     location=[-7.258765, 112.750797],
     popup='PT Mastrada Surya',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.256224, 112.752060],
     popup='GBI Ambengan',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.258337, 112.747794],
     popup='Yayasan Kas Pembangunan',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.257641, 112.747492],
     popup='Kantor Pemerintah Kota Surabaya',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.258379, 112.748569],
     popup='Asoka',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.255759, 112.748879],
     popup='Princess Photo Studio Surabaya',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.253498, 112.750316],
     popup='Taman Remaja Surabaya',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.252583, 112.749436],
     popup='Apotik Kusuma',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
 folium.Marker(
     location=[-7.256744, 112.750813],
     popup='Ganesha Operation',
-    icon=folium.Icon(color='orange', icon='info-sign')
+    icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
-
-bocahmicin = folium.Map(
-    location=[-7.3044144,112.7412095],
-    zoom_start=12,
-    tiles='Stamen Terrain'
-)
 
 folium.Marker(
     location=[-7.219674, 112.713401],
@@ -708,11 +693,6 @@ folium.Marker(
     icon=folium.Icon(icon='cloud')
 ).add_to(bocahmicin)
 
-bocahmicin = folium.Map(
-    location=[-7.3044144, 112.7412095],
-    zoom_start=12,
-    tiles='Stamen Terrain'
-)
 folium.Marker(
     location=[-7.282336, 112.792975],
     popup='Masjid Manarul Ilmi',
